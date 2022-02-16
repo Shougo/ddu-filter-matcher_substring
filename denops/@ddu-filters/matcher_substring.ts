4f5109aca2d3f10d6f0ffe258f79873aa8c51ae2
Shougo Matsushita <Shougo.Matsu@gmail.com>
2022-02-16 14:21:22 +0900

    Optimize performance

--- denops/@ddu-filters/matcher_substring.ts
+++ denops/@ddu-filters/matcher_substring.ts
@@ -1,5 +1,9 @@
-import { BaseFilter, DduItem, SourceOptions } from "https://deno.land/x/ddu_vim@v0.1.0/types.ts";
-import { Denops } from "https://deno.land/x/ddu_vim@v0.1.0/deps.ts";
+import {
+  BaseFilter,
+  DduItem,
+  SourceOptions,
+} from "https://deno.land/x/ddu_vim@v0.13/types.ts";
+import { Denops } from "https://deno.land/x/ddu_vim@v0.13/deps.ts";
 
 type Params = Record<never, never>;
 
@@ -10,15 +14,20 @@ export class Filter extends BaseFilter<Params> {
     input: string;
     items: DduItem[];
   }): Promise<DduItem[]> {
+    if (args.input == "") {
+      return Promise.resolve(args.items);
+    }
+
     const input = args.sourceOptions.ignoreCase
       ? args.input.toLowerCase()
       : args.input;
-    return Promise.resolve(args.items.filter(
-      (item) =>
-        args.sourceOptions.ignoreCase
-          ? item.matcherKey.toLowerCase().includes(input)
-          : item.matcherKey.includes(input),
-    ));
+    return args.sourceOptions.ignoreCase
+      ? Promise.resolve(args.items.filter(
+        (item) => item.matcherKey.toLowerCase().includes(input),
+      ))
+      : Promise.resolve(args.items.filter(
+        (item) => item.matcherKey.includes(input),
+      ));
   }
 
   params(): Params {
