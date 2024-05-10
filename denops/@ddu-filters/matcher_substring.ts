2b47c8380d650ee0bf74f1f3c99928732acccd15
Shougo Matsushita <Shougo.Matsu@gmail.com>
2024-05-10 14:04:22 +0900

    Update deps

--- denops/@ddu-filters/matcher_substring.ts
+++ denops/@ddu-filters/matcher_substring.ts
@@ -2,8 +2,8 @@ import {
   BaseFilter,
   DduItem,
   SourceOptions,
-} from "https://deno.land/x/ddu_vim@v3.4.6/types.ts";
-import { Denops } from "https://deno.land/x/ddu_vim@v3.4.6/deps.ts";
+} from "https://deno.land/x/ddu_vim@v4.0.0/types.ts";
+import { Denops } from "https://deno.land/x/ddu_vim@v4.0.0/deps.ts";
 
 type Params = {
   highlightMatched: string;
